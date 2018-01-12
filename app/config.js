899de51f71e2bcb99127abb5289605c98cddd14e
gavinelliott <gavin@gavinelliott.co.uk>
2018-01-12 14:25:09 +0000

    MVP Build

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Verify Earnings and Pensions',
 
   // Default port that prototype runs on
   port: '3000',
